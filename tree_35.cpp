e4c384b7d6dab629242141c0d9a60a0a2377b63b
yashkapoor007 <ykapoor_be15@thapar.edu>
2018-02-25 00:40:09 +0530

    better level order

---
+++ tree_35.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<map>
+#include "btree.h"
+using namespace std;
+Btree<int>* takeInputLevelWise()
+{
+  int rootdata;
+  cout<<"enter root data"<<endl;
+  cin>>rootdata;
+  if(rootdata==-1)
+  return NULL;
+  Btree<int>* root=new Btree<int>(rootdata);
+  queue<Btree<int>* > pendingnodes;
+  pendingnodes.push(root);
+  // int i=0;
+  while(pendingnodes.size()>0)
+  {
+    Btree<int>* front=pendingnodes.front();
+    pendingnodes.pop();
+    int lc;
+    int rc;
+    cout<<"enter the left child of "<<front->data<<endl;
+    cin>>lc;
+    if(lc!=-1)
+    {
+      Btree<int>* leftchild=new Btree<int>(lc);
+      front->left=leftchild;
+      pendingnodes.push(leftchild);
+    }
+    cout<<"enter the right child of "<<front->data<<endl;
+    cin>>rc;
+    if(rc!=-1)
+    {
+      Btree<int>* rightchild=new Btree<int>(rc);
+      front->right=rightchild;
+      pendingnodes.push(rightchild);
+    }
+    // i++;
+    // cout<<i<<endl;
+  }
+  return root;
+}
+void levelOrder(Btree<int>* node)
+{
+  //Your code here
+  if(node==NULL)
+  return;
+  queue<Btree<int>* > q;
+  q.push(node);
+  while(q.size()>0)
+  {
+      int count=q.size();
+      while(count--)
+      {
+      Btree<int>* front=q.front();
+      q.pop();
+      cout<<front->data<<" ";
+      if(front->left)
+      q.push(front->left);
+      if(front->right)
+      q.push(front->right);
+      }
+      cout<<"$ ";
+  }
+}
+void printInorder(Btree<int>* root)
+{
+  if(root==NULL)
+  return;
+  printInorder(root->left);
+  cout<<root->data<<" ";
+  printInorder(root->right);
+}
+
+int main()
+{
+  cout<<"Input tree"<<endl;
+    Btree<int> *root=takeInputLevelWise();
+    levelOrder(root);
+    cout<<endl;
+}
