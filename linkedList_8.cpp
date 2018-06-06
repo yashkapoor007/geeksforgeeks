59dba38593a978462be9ea0f5a1e4e1847732f9c
yashkapoor007 <ykapoor_be15@thapar.edu>
2018-06-06 23:05:45 +0530

    find a given number

---
+++ linkedList_8.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include "Node.cpp"
+using namespace std;
+Node* buildList()
+{
+  Node* head=NULL;
+  Node* tail=NULL;
+  int data=0;
+  while(data!=-1)
+  {
+    cin>>data;
+    if(data!=-1)
+    {
+      Node* newnode=new Node(data);
+      if(head==NULL)
+      {head=newnode;
+        tail=newnode;
+      }
+      else if(head!=NULL)
+      {
+          tail->next=newnode;
+          tail=tail->next;
+      }
+    }
+
+  }
+  return head;
+}
+Node* deleteANode(Node* head,int x)
+{
+  Node* temp=head;
+    if(x==1)
+    {
+        temp=head;
+        head=head->next;
+        delete temp;
+    }
+    else
+    {
+      for(int i=1;i<x-1;i++)
+      {
+        temp=temp->next;
+      }
+      Node* temp1=temp->next;
+      temp->next=temp1->next;
+      delete temp1;
+    }
+    return head;
+}
+void print(Node* head)
+{
+  Node* temp=head;
+  while(temp!=NULL)
+  {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+  cout<<endl;
+}
+int length(Node* head)
+{
+  if(head==NULL)
+  return 0;
+  return 1+length(head->next);
+}
+int search(Node* head,int &i,int x)
+{
+  if(head==NULL)
+  {
+    i=-1;
+    return i;
+  }
+  if(head->data==x)
+  return i;
+  else
+  {
+    i++;
+    search(head->next,i,x);
+  }
+}
+int main()
+{
+  Node* head=buildList();
+  print(head);
+  int x;
+  cout<<"Enter the number to be searched"<<endl;
+  cin>>x;
+  int i=0;
+  cout<<"found at "<<search(head,i,x)<<endl;
+}
