68b7c9d0de2577dfda4b93bc3c0a1236bdaf5cdc
yashkapoor007 <ykapoor_be15@thapar.edu>
2018-06-07 21:18:34 +0530

    print a list reverse with recursion

---
+++ linkedList_23.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include "Node.cpp"
+using namespace std;
+Node* buildList()
+{
+  Node* head=NULL;
+  Node* tail=NULL;
+  int data=0;
+  while(data!=-1)
+  {
+    cin>>data;
+    if(data!=-1)
+    {
+      Node* newnode=new Node(data);
+      if(head==NULL)
+      {head=newnode;
+        tail=newnode;
+      }
+      else if(head!=NULL)
+      {
+          tail->next=newnode;
+          tail=tail->next;
+      }
+    }
+
+  }
+  return head;
+}
+
+Node* deleteANode(Node* head,int x)
+{
+  Node* temp=head;
+    if(x==1)
+    {
+        temp=head;
+        head=head->next;
+        delete temp;
+    }
+    else
+    {
+      for(int i=1;i<x-1;i++)
+      {
+        temp=temp->next;
+      }
+      Node* temp1=temp->next;
+      temp->next=temp1->next;
+      delete temp1;
+    }
+    return head;
+}
+Node* reverseAlinkedList(Node* head,Node* prev=NULL)
+{
+  if(head==NULL)
+  {
+    head=prev;
+    return prev;
+  }
+  Node* next=head->next;
+  head->next=prev;
+  reverseAlinkedList(next,head);
+}
+void print(Node* head)
+{
+  if(head==NULL)
+  cout<<"empty"<<endl;
+  Node* temp=head;
+  while(temp!=NULL)
+  {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+  cout<<endl;
+}
+void printreverse(Node* head)
+{
+  if(head==NULL)
+  {
+    return;
+  }
+  printreverse(head->next);
+  cout<<head->data<<" ";
+}
+int main()
+{
+  Node* head=buildList();
+  print(head);
+  printreverse(head);
+  cout<<endl;
+}
